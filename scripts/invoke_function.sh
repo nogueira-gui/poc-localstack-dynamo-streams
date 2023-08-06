eb0c3f2cb9265677fc7f8806c486653c717c2db2
Guilherme Nogueira <guilherme.nogueiras@outlook.com>
2023-08-06 20:32:39 -0300

    refactor: create payload.json file

--- scripts/invoke_function.sh
+++ scripts/invoke_function.sh
@@ -5,6 +5,12 @@ FUNCTION_NAME="lbd-search-file"
 export AWS_ACCESS_KEY_ID=my_access_key
 export AWS_SECRET_ACCESS_KEY=my_secret_key
 
+cat > payload.json <<EOF
+{
+    "file_key": "cloud_practioner_1.json"
+}
+EOF
+
 echo "Invoking $FUNCTION_NAME"
 aws --endpoint-url $AWS_ENDPOINT_URL lambda invoke \
     --function-name $FUNCTION_NAME \
